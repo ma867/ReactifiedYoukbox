d731ce861791d2b18f16c98af186dca6803da865
ma867 <ma867@njit.edu>
2023-03-24 01:16:36 -0400

    triggering heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 })
 
 app.use(logger('dev'))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 
