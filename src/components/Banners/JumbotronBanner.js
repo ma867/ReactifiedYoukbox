06eb8bf3f2ef53ba2b7d549756cfbc02483e87b8
ma867 <ma867@njit.edu>
2023-03-28 12:37:56 -0400

    test for iphone screen

--- src/components/Banners/JumbotronBanner.js
+++ src/components/Banners/JumbotronBanner.js
@@ -3,7 +3,7 @@ import DarkButton from '../Buttons/DarkButton'
 export default function JumbotronBanner({ setAuth }) {
     return (
         <div className='jumbotron-banner'>
-            <div className="mix-blend-mode jumbotron-banner-icon p-3" />
+            <div className="mix-blend-mode jumbotron-banner-icon" />
             <div className='section-header  p-3'>
                 <h2 className='jumbotron-banner-title display-3  p-3  '>
                     <span className='title light'>
